2a0268208aca3d13f8f9be371852549764470581
STIWAR JULIAN <STIWAROSPINA6@GMAIL.COM>
2020-05-09 07:42:25 -0500

    Configuracion inicial y migraciones

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		factory(App\Models\User::class, 50)->create();
+	}
+}
